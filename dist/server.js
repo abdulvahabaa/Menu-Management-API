0eec3774e04b090dc8996f40b96f6b33f4622f37
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-01-27 19:32:50 +0530

    test:build file

--- dist/server.js
+++ dist/server.js
@@ -10,16 +10,12 @@ const helmet_1 = __importDefault(require("helmet"));
 const menus_routes_1 = __importDefault(require("./routes/menus.routes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const corsOptions = {
-    origin: "http://localhost:9000",
-    methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS"],
-};
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, helmet_1.default)());
 app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use((0, cors_1.default)(corsOptions));
-app.use("/api/v1/menus", menus_routes_1.default);
+app.use((0, cors_1.default)());
+app.use("/menus", menus_routes_1.default);
 app.use((req, res, next) => {
     res.status(404).json({ message: "Route not Found" });
 });
